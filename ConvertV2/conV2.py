84fe5a1abb7a88ea32953a7fbaa5febf4513aef9
maulana <maulanaauliyafirdaus11@gmail.com>
2025-05-28 10:29:29 +0700

    Add files via upload
    sesi 12

---
+++ ConvertV2/conV2.py
@@ -0,0 +1,8 @@
+def desToBin(desimal):
+    return bin(desimal)
+
+def desToOkta(desimal):
+    return oct(desimal)
+
+def desToHex(desimal):
+    return hex(desimal)
---
+++ Mathematic/math.py
@@ -0,0 +1,15 @@
+#Untuk di panggil dengan powNumber(2,3) hasilnya akan 8
+def powNumber(num1,num2): 
+    return num1 ** num2
+
+def addNumber(num1,num2):
+    return num1 + num2
+
+def subNumber(num1,num2):
+    return num1 - num2
+
+def mulNumber(num1,num2):
+    return num1 * num2
+
+def divNumber(num1,num2):
+    return num1 / num2
